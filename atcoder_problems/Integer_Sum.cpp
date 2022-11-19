eca8c9b2293a9fa3de3e23aa0a85ad70b0f49610
taketo <take10tokyohkit@gmail.com>
2022-11-19 17:05:41 +0900

    AC

---
+++ atcoder_problems/Integer_Sum.cpp
@@ -0,0 +1,19 @@
+// AC
+
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep2(i, s, n) for (int i = (s); i < (int)(n); i++)
+#define REP(i,a,b) for(int i = (a); i < (b); i++)
+#define MOD 1000000007
+
+int main(void) {
+  int n;
+  cin >> n;
+  vector<int> a(n);
+  rep(i,n) cin >> a[i];
+  int res=0;
+  rep(i,n) res += a[i];
+  cout << res << endl;
+}
