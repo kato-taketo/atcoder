2dc13eed9f20a5424b9480ba011bc54507e7f65e
taketo <take10tokyohkit@gmail.com>
2022-11-24 16:54:40 +0900

    official commentary

--- atcoder_problems/D_-_Unique_Username.cpp
+++ atcoder_problems/D_-_Unique_Username.cpp
@@ -1,31 +1,42 @@
 #include <bits/stdc++.h>
 using namespace std;
-using ll = long long;
-#define rep(i, n) for (int i = 0; i < (int)(n); i++)
-#define rep2(i, s, n) for (int i = (s); i < (int)(n); i++)
-#define REP(i,a,b) for(int i = (a); i < (b); i++)
-#define MOD 1000000007
 
-void func() {
-  int res=0;
-  for(int stop=8; stop>=1; stop--) {
-    int tmp=1;
-    for(int i=8; i>=stop; i--) {
-      tmp *= i;
+void dfs(int cur, vector<string> &S, vector<string> &T, int remain, string ans) {
+  if(remain<0) return ;
+  if(cur == S.size()) {
+    if(ans.size()>=3 && !binary_search(T.begin(),T.end(),ans)) {
+      cout << ans << endl;
+      exit(0);
     }
-    res += tmp;
+    return ;
   }
-  cout << res << endl;
-  //return res;
+  if(ans.size()>0 && ans.back()!='_') {
+    dfs(cur,S,T,remain,ans+"_");
+  } else {
+    dfs(cur+1,S,T,remain,ans+S[cur]);
+    if(ans.size()>0) dfs(cur,S,T,remain-1,ans+"_");
+  }
 }
-int main(void) {
-  func();
+
+int main() {
   int N,M;
   cin >> N >> M;
-  vector<string> S(N);
-  rep(i,N) cin >> S[i];
-  vector<string> T(N);
-  rep(i,N) cin >> T[i];
 
-  // 文字の並べ方は、
+  vector<string > S(N);
+  for(int i=0; i<N; i++) cin >> S[i];
+  sort(S.begin(),S.end());
+
+  vector<string> T(M);
+  for(int i=0; i<M; i++) cin >> T[i];
+  sort(T.begin(),T.end());
+
+  int remain=16;
+  for(int i=0; i<N;i++) remain-=S[i].size();
+  remain -= N-1;
+
+  do {
+    dfs(0,S,T,remain,"");
+  } while(next_permutation(S.begin(),S.end()));
+
+  cout << -1 << endl;
 }
