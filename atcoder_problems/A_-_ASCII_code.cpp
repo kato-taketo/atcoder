18f5d27a19660231308b99ffaae55b9b5d89993c
taketo <take10tokyohkit@gmail.com>
2022-12-22 16:44:19 +0900

    AC

---
+++ atcoder_problems/A_-_ASCII_code.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep2(i, s, n) for (int i = (s); i < (int)(n); i++)
+#define REP(i,a,b) for(int i = (a); i < (b); i++)
+#define MOD 1000000007
+
+int main(void) {
+  int n;
+  cin >> n;
+  printf("%c\n", char(n));
+}
