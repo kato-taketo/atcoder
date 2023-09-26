9c035b9b8b998ada1097d83f7ac1bfaa74f6787d
taketo <take10tokyohkit@gmail.com>
2023-09-26 13:48:15 +0900

    b56 done

---
+++ tessoku2/08/56.cpp
@@ -0,0 +1,80 @@
+// 13:27~
+// 回文かどうかを単純に探索したら
+// o(Q N/2)
+/*
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep2(i, s, n) for (int i = (s); i < (int)(n); i++)
+#define REP(i,a,b) for(int i = (a); i < (b); i++)
+#define MOD 1000000007
+
+int N,Q;
+string S;
+vector<int> L,R;
+int main(void) {
+  cin >> N >> Q;
+  cin >> S;
+  L.resize(Q); R.resize(Q);
+  rep(i,Q) cin >> L[i] >> R[i];
+
+}
+*/
+
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+int N,Q,L[100009],R[100009];
+string S;
+string SRev;
+
+int T[100009];
+int TRev[100009];
+
+long long mod = 2147483647;
+long long Power100[100009];
+long long H[100009];
+long long HRev[100009];
+
+long long GetHashLeft(int l, int r) {
+  long long val = H[r] - (Power100[r-l+1]*H[l-1]%mod);
+  if (val < 0 ) val += mod;
+  return val;
+}
+
+long long GetHashRight(int l, int r) {
+  int true_l = N+1-r;
+  int true_r = N+1-l;
+  long long val = HRev[true_r] - (Power100[true_r-true_l+1]*HRev[true_l-1] % mod);
+  if(val < 0 ) val += mod;
+  return val;
+}
+int main() {
+  cin >> N >> Q;
+  cin >> S;
+  for (int i=1; i<=Q; i++) cin >> L[i] >> R[i];
+  SRev = S;
+  reverse(SRev.begin(), SRev.end());
+
+  for (int i=1; i<=N; i++ ) T[i] = (int)(S[i-1]-'a') + 1;
+  for (int i=1; i<=N; i++) TRev[i] = (int)(SRev[i-1]-'a') + 1;
+
+  Power100[0] = 1;
+  for (int i=1; i<=N; i++ ) Power100[i] = (100LL * Power100[i-1]) % mod;
+
+  H[0] = 1;
+  for (int i=1; i<=N; i++) H[i] = (100LL * H[i-1] + T[i]) % mod;
+
+  HRev[0] = 1;
+  for (int i=1; i<=N; i++) HRev[i] = (100LL * HRev[i-1] + TRev[i]) % mod;
+
+  for (int i=1; i<=Q; i++) {
+    long long v1 = GetHashLeft(L[i], R[i]);
+    long long v2 = GetHashRight(L[i], R[i]);
+    if(v1 == v2) cout << "Yes" << endl;
+    else cout << "No" << endl;
+  }
+}
