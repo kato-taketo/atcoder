b94e1f7bc37a2eb4989618f76df1d057029f8fa5
taketo <take10tokyohkit@gmail.com>
2023-12-04 16:10:19 +0900

    after contest

--- ABC/abc331/e.cpp
+++ ABC/abc331/e.cpp
@@ -5,41 +5,49 @@ using ll = long long;
 #define rep2(i, s, n) for (int i = (s); i < (int)(n); i++)
 #define REP(i,a,b) for(int i = (a); i < (b); i++)
 #define MOD 1000000007
-#define inf 2e9
-bool compare(int a, int b) {
-    return a > b; // 大きい順にソートするための条件
-}
-int main(void) {
-  int n,m,l;
-  cin >> n >> m >> l;
-  vector<ll> a(n),b(m);
-  rep(i,n) cin >> a[i];
-  rep(i,m) cin >> b[i];
-  vector<ll> c(l),d(l);
-  rep(i,l) cin >> c[i] >> d[i];
 
-  map<pair<int,int>, bool> mp;  // ci, di, のペアを格納
-  vector<ll> value;
-  vector<pair<ll,ll> > aa(n), bb(m);
-  vector<pair<ll, pair<ll,ll> > > vec_pair;
-  rep(i,l) mp[make_pair(c[i],d[i])]=true;
-  rep(i,n) aa[i] = make_pair(a[i],i+1);
-  rep(i,m) bb[i] = make_pair(b[i],i+1);
-  sort(aa.rbegin(),aa.rend());
-  sort(bb.rbegin(),bb.rend());
-  int x=0,y=0;
-  while(x<n&&y<m) {
-    pair<int,int> P=make_pair(aa[x].second,bb[y].second);
-    ll value = aa[x].first+bb[y].first;
-    if(!mp[P]) {
-      cout << value << endl; return 0;
-    }
-    vec_pair.push_back(make_pair(aa[x].first+bb[y].first, make_pair(x+1,y+1)));
-    ll dif_x=inf,dif_y=inf;
-    if(x+1<n) dif_x = aa[x].first-aa[x+1].first;
-    if(y+1<m) dif_y = bb[y].first-bb[y+1].first;
-    if(dif_x>dif_y) y++;
-    else x++;
+int N,M,L;
+vector<ll> cur,b;
+vector<pair<int,int> > a_index,b_index;
+map<pair<int,int>, bool> mp_cd;
+priority_queue<pair<int,int> > pq;  // value, index of b
+int main(void) {
+  cin >> N >> M >> L;
+  a_index.resize(N); b_index.resize(M);
+  b.resize(M);
+  rep(i,N) {
+    int a; cin >> a;
+    a_index[i]=make_pair(a,i);
   }
-  
+  rep(i,M) {
+    cin >> b[i];
+    //int b; cin >> b;
+    //b_index[i]=make_pair(b,i);
+  }
+  rep(i,L) {
+    int c,d;
+    cin >> c >> d;
+    mp_cd[make_pair(c,d)]=true;
+  }
+  cur.resize(M,0);  // cur[i] := 副菜iは、主菜cur[i]までみた
+  sort(a_index.rbegin(), a_index.rend()); 
+  sort(b_index.rbegin(), b_index.rend());
+  rep(i,M) pq.push(make_pair(a_index[0].first+b[i],i));
+
+  // search
+  while(!pq.empty()) {
+    pair<int,int> node=pq.top();
+    pq.pop();
+    int b_now,a_now;
+    b_now=node.second;
+    a_now=a_index[cur[b_now]].second;
+    //cout << "a: " << a_now+1 << " b: " << b_now+1 << endl;
+    if(mp_cd[make_pair(a_now+1,b_now+1)]==false) {
+      cout << node.first << endl; return 0;
+    }
+    if(cur[b_now]!=N-1) {
+      cur[b_now]++;
+      pq.push(make_pair(a_index[cur[b_now]].first+b[b_now], b_now));
+    }
+  }
 }
